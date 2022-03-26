74a873b3852f799c30a131fef6383f9322a63dbf
Joao Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-03-26 11:51:55 +0000

    compute knobs dynamically

--- operator.lua
+++ operator.lua
@@ -3,8 +3,7 @@
 -- desc:   RetroJam 2022 organized by IEEE UP SB
 -- script: lua
 -- Viewport 240x136
--- Switchboard properties
-sb = {
+SWITCHBOARD = {
     start_x = 10,
     start_y = 10,
     row_num = 4,
@@ -13,15 +12,35 @@ sb = {
     row_spacing = 25
 }
 
-t = 0
+FRAME_COUNTER = 0
+
+-- knobs are computed based on switch board params
+KNOBS = {}
+KNOB_STATE = {OFF = "off", INCOMING = "incoming", CONNECTED = "connected"}
 
 function TIC()
     update()
     draw()
 end
 
+-- inits
+function init() KNOBS = get_knobs() end
+
+function get_knobs()
+    local knobs = {}
+    for i = 0, SWITCHBOARD.row_num - 1 do
+        for j = 0, SWITCHBOARD.col_num - 1 do
+            x = SWITCHBOARD.start_x + (j * SWITCHBOARD.col_spacing)
+            y = SWITCHBOARD.start_y + (i * SWITCHBOARD.row_spacing)
+            knob = {x = x, y = y, state = KNOB_STATE.OFF, timer = 0}
+            table.insert(knobs, knob)
+        end
+    end
+    return knobs
+end
+
 -- updates
-function update() t = t + 1 end
+function update() FRAME_COUNTER = FRAME_COUNTER + 1 end
 
 -- draws
 function draw()
@@ -29,23 +48,28 @@ function draw()
     rectb(0, 0, 240, 136, 2)
     draw_switchboard()
     draw_message_box()
-    draw_knob(sb.start_x, sb.start_y)
+    -- draw_knob(SWITCHBOARD.start_x, SWITCHBOARD.start_y)
+
+    draw_knob(KNOBS[5].x, KNOBS[5].y)
+
 end
 
 function draw_switchboard()
-    rectb(5, 5, (sb.col_num * sb.col_spacing) - 8, sb.row_num * sb.row_spacing,
-          1)
-    for i = 0, sb.row_num - 1, 1 do
-        for j = 0, sb.col_num - 1, 1 do
-            spr(0, sb.start_x + (j * sb.col_spacing),
-                sb.start_y + (i * sb.row_spacing), -1, 2)
+    rectb(5, 5, (SWITCHBOARD.col_num * SWITCHBOARD.col_spacing) - 8,
+          SWITCHBOARD.row_num * SWITCHBOARD.row_spacing, 1)
+    for i = 0, SWITCHBOARD.row_num - 1 do
+        for j = 0, SWITCHBOARD.col_num - 1 do
+            spr(0, SWITCHBOARD.start_x + (j * SWITCHBOARD.col_spacing),
+                SWITCHBOARD.start_y + (i * SWITCHBOARD.row_spacing), -1, 2)
         end
     end
 end
 
-function draw_message_box() rectb(5, sb.row_num * sb.row_spacing + 8, 230, 25, 5) end
+function draw_message_box()
+    rectb(5, SWITCHBOARD.row_num * SWITCHBOARD.row_spacing + 8, 230, 25, 5)
+end
 
-function draw_knob(x, y) spr(0 + t % 60 // 30 * 2, x, y, -1, 2) end
+function draw_knob(x, y) spr(0 + FRAME_COUNTER % 60 // 30 * 2, x, y, -1, 2) end
 
 -- utils
 function has_value(tab, val)
@@ -60,6 +84,9 @@ function ifthenelse(cond, t, f)
         return f
     end
 end
+
+-- starts the game
+init()
 
 -- <TILES>
 -- 000:00ffff000ffeeff0ffeeeefffeeeeedffeeeeedfffeeedff0ffedff000ffff00
